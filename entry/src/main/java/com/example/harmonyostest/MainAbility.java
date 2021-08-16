8edf862d3a13bbf3fb3a86dc0687a321d78c49ad
coolcoolyisuanwang <yisuanwang@gmail.com>
2021-08-16 21:45:13 +0800

    Initial commit

---
+++ entry/src/main/java/com/example/harmonyostest/MainAbility.java
@@ -0,0 +1,24 @@
+package com.example.harmonyostest;
+
+import com.example.harmonyostest.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main); // 加载XML布局
+//        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
---
+++ entry/src/test/java/com/example/harmonyostest/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.harmonyostest;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
